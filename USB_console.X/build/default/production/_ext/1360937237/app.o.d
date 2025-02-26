16bce2a18a37cd87f39b0b20643c8dd0a3215ed3
PICON Maxime <maxime.picon@snef.fr>
2025-02-26 11:46:14 +0100

    Initialisation du dépôt et ajout de la structure de dossiers et des fichiers sources

---
+++ USB_console.X/build/default/production/_ext/1360937237/app.o.d
@@ -0,0 +1,50 @@
+build/default/production/_ext/1360937237/app.o: ../src/app.c ../src/app.h \
+ ../src/config/default/configuration.h ../src/config/default/user.h \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/peripheral/uart/plib_uart6.h \
+ ../src/config/default/device.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h \
+ ../src/config/default/bsp/bsp.h \
+ ../src/config/default/system/console/sys_console.h \
+ ../src/config/default/system/system.h \
+ ../src/config/default/system/system_common.h \
+ ../src/config/default/system/system_module.h \
+ ../src/config/default/system/system_common.h \
+ ../src/config/default/driver/driver.h \
+ ../src/config/default/driver/driver_common.h \
+ ../src/config/default/system/console/src/sys_console_local.h
+
+../src/app.h:
+
+../src/config/default/configuration.h:
+
+../src/config/default/user.h:
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/peripheral/uart/plib_uart6.h:
+
+../src/config/default/device.h:
+
+../src/config/default/peripheral/uart/plib_uart_common.h:
+
+../src/config/default/bsp/bsp.h:
+
+../src/config/default/system/console/sys_console.h:
+
+../src/config/default/system/system.h:
+
+../src/config/default/system/system_common.h:
+
+../src/config/default/system/system_module.h:
+
+../src/config/default/system/system_common.h:
+
+../src/config/default/driver/driver.h:
+
+../src/config/default/driver/driver_common.h:
+
+../src/config/default/system/console/src/sys_console_local.h:
